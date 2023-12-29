277d5e29b9431902b9f66109d7132c7b0f3cc1c7
alezi <pangjianye@163.com>
2023-12-29 21:45:50 +0800

    1

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
+const path = require('path');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// 解析 JSON 请求体
 app.use(express.json());
 
+// 提供静态文件，'public' 是包含 index.html 的目录
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// 处理根 URL 请求并提供 index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-
-  
-
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // 处理域名查询的 API 请求
 app.post('/query-domain', async (req, res) => {
